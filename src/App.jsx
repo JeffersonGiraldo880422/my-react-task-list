56f84b6913461426c01bb260cb7d599a190c53f5
jaga88 <jaga88@gmail.com>
2023-06-16 20:21:21 -0500

    App con el uso de useState, useEffect y localStorage en la branch project-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Entrada from './components/Entrada';
 import TaskList from './components/TaskList';
@@ -9,8 +9,11 @@ import './App.css';
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  
   const handleAddTask = (newTask) => {
     setTasks([...tasks, newTask]);
+    let storedTasks = [...tasks];
+    localStorage.setItem("storedTasksMemory",JSON.stringify(storedTasks));
   };
 
   const handleToggleTask = (id) => {
@@ -22,15 +25,32 @@ function App() {
         };
       }
       return task;
+
     });
 
     setTasks(updatedTasks);
+    storedTasks = [...tasks];
+    localStorage.setItem("storedTasksMemory",JSON.stringify(storedTasks));
   };
 
   const handleDeleteCompletedTasks = () => {
     const remainingTasks = tasks.filter((task) => !task.completed);
     setTasks(remainingTasks);
+    storedTasks = [...tasks];
+    localStorage.setItem("storedTasksMemory",JSON.stringify(storedTasks));
   };
+
+  useEffect (() => {
+    const localStorageData = localStorage.getItem("storedTasksMemory");
+    if (localStorageData) {
+      try{
+        const storedTasks = JSON.parse(localStorageData);
+        setTasks(storedTasks);
+      }catch(err){
+        console.err("Error parsing stored tasks memory from localStorage")
+      }
+    }
+  },[]);
 
   return (
     <div className="app">
