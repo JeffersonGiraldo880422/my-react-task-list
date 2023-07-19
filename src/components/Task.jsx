1ad333c753df454313156c4735d90a41380abcb8
jaga88 <jaga88@gmail.com>
2023-07-18 23:12:46 -0500

    Optimizando la app con el us de form

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaEdit } from 'react-icons/fa';
 
 function Task({ task, onToggleTask, onUpdateTask }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(task.title);
+  const [description, setDescription] = useState(task.description);
+
   const handleToggle = () => {
     onToggleTask(task.id);
   };
 
   const handleEdit = () => {
-    const updatedTitle = prompt("Ingrese el nuevo título de la tarea:", task.title);
-    if (updatedTitle !== null) {
-      onUpdateTask(task.id, updatedTitle);
+    setIsEditing(true);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setTitle(task.title);
+    setDescription(task.description);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim().length < 3) {
+      alert("El nombre de la tarea debe tener al menos 3 caracteres.");
+      return;
     }
+
+    onUpdateTask(task.id, title, description);
+    setIsEditing(false);
   };
 
   return (
@@ -20,8 +38,27 @@ function Task({ task, onToggleTask, onUpdateTask }) {
         checked={task.completed}
         onChange={handleToggle}
       />
-      <span>{task.title}</span>
-      <FaEdit className="edit-icon" onClick={handleEdit} />
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <button type="submit">Guardar</button>
+          <button type="button" onClick={handleCancel}>Cancelar</button>
+        </form>
+      ) : (
+        <div>
+          <span>{task.title}</span>
+          <span>{task.description}</span>
+          <FaEdit className="edit-icon" onClick={handleEdit} />
+        </div>
+      )}
     </li>
   );
 }
