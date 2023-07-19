fb24891932e8cf036dc51cda55843157cb6a0c7d
jaga88 <jaga88@gmail.com>
2023-07-18 21:30:09 -0500

    Mejorando el código App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import Header from './components/Header';
-import Entrada from './components/Entrada';
-import TaskList from './components/TaskList';
-import Eliminar from './components/Eliminar';
+import React, { useState, useEffect } from 'react';
+import useTasks from './useTasks';
 
-import './App.css';
-
-function App() {
+const App = () => {
   const [tasks, setTasks] = useState([]);
+  const { createTask, deleteTask, updateTask } = useTasks();
 
   const handleAddTask = (newTask) => {
-    const updatedTasks = [...tasks, newTask];
-    setTasks(updatedTasks);
-    localStorage.setItem("storedTasksMemory", JSON.stringify(updatedTasks));
+    if (newTask.trim() !== '') {
+      createTask(newTask);
+    }
   };
 
   const handleToggleTask = (id) => {
@@ -25,28 +21,13 @@ function App() {
       }
       return task;
     });
-
     setTasks(updatedTasks);
-    localStorage.setItem("storedTasksMemory", JSON.stringify(updatedTasks));
   };
 
   const handleDeleteCompletedTasks = () => {
     const remainingTasks = tasks.filter((task) => !task.completed);
     setTasks(remainingTasks);
-    localStorage.setItem("storedTasksMemory", JSON.stringify(remainingTasks));
   };
-
-  useEffect(() => {
-    const localStorageData = localStorage.getItem("storedTasksMemory");
-    if (localStorageData) {
-      try {
-        const storedTasks = JSON.parse(localStorageData);
-        setTasks(storedTasks);
-      } catch (err) {
-        console.error("Error parsing stored tasks memory from localStorage");
-      }
-    }
-  }, []);
 
   return (
     <div className="app">
@@ -56,6 +37,6 @@ function App() {
       <Eliminar onDeleteCompletedTasks={handleDeleteCompletedTasks} />
     </div>
   );
-}
+};
 
 export default App;
